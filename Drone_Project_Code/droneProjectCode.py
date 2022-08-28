f14f48fbd2708238b4647cb1da74168c30e34de7
Mpilo Mbulelo Tatana <81185746+Artificial-Stupidity-Lab@users.noreply.github.com>
2022-08-28 04:22:26 +0200

    window functions created

--- Drone_Project_Code/droneProjectCode.py
+++ Drone_Project_Code/droneProjectCode.py
@@ -24,35 +24,68 @@ frame.place(relwidth=1,relheight=1)
 myFont1 = font.Font(family='Helvetica', size=20, weight='bold')
 #GUI variables
 mode = IntVar()
+mode.set(0)
+
+#function for clicked button
+def get_battery(event):
+    pass
+    #create mesage box
+
+
+def modeSel():
+    if(mode.get()==0):
+        mode0Win()
+    elif(mode.get()==1):
+        mode1Win()
+    elif(mode.get()==2):
+        mode2Win()
+    elif(mode.get()==3):
+        mode3Win()
+    elif(mode.get()==4):
+        mode4Win()
+
+#opening new windows for the each mode
+def mode0Win():
+    pass
+def mode1Win():
+    pass
+def mode2Win():
+    pass
+def mode3Win():
+    pass
+def mode4Win():
+    pass
+
 
 
 #Surveillance Mode
-btn_gate1_open = Radiobutton(root, text="Surveillance", bg="lime", variable=mode, value=1)
-btn_gate1_open['font'] = myFont1
-btn_gate1_open.place(relx=0, rely=0, relwidth=0.5, relheight=0.2)
+btn_surveillance = Radiobutton(root, text="Surveillance", bg="lime", variable=mode, value=1, command=modeSel)
+btn_surveillance['font'] = myFont1
+btn_surveillance.place(relx=0, rely=0, relwidth=0.5, relheight=0.2)
 #Voice Control Mode
-btn_gate1_close = Radiobutton(root, text="Voice Control", bg="yellow", variable=mode, value=2)
-btn_gate1_close['font'] = myFont1
-btn_gate1_close.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.2)
+btn_voice = Radiobutton(root, text="Voice Control", bg="yellow", variable=mode, value=2,command=modeSel)
+btn_voice['font'] = myFont1
+btn_voice.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.2)
 
 #Hand Control Mode
-btn_gate2_open = Radiobutton(root, text="Hand Control", bg="orange", variable=mode, value=3)
-btn_gate2_open['font'] = myFont1
-btn_gate2_open.place(relx=0, rely=0.3, relwidth=0.5, relheight=0.2)
+btn_hand = Radiobutton(root, text="Hand Control", bg="orange", variable=mode, value=3,command=modeSel)
+btn_hand['font'] = myFont1
+btn_hand.place(relx=0, rely=0.3, relwidth=0.5, relheight=0.2)
 #Tracking Mode
-btn_gate2_close = Radiobutton(root, text="Tracking", bg="#80c1ff",variable=mode, value=4)
-btn_gate2_close['font'] = myFont1
-btn_gate2_close.place(relx=0.5, rely=0.3, relwidth=0.5, relheight=0.2)
+btn_tracking = Radiobutton(root, text="Tracking", bg="#80c1ff",variable=mode, value=4,command=modeSel)
+btn_tracking['font'] = myFont1
+btn_tracking.place(relx=0.5, rely=0.3, relwidth=0.5, relheight=0.2)
 
 #Battery
-btn_available_parking = tk.Button(root, text="Get Battery", bg="violet")
-btn_available_parking["font"] = myFont1
-btn_available_parking.place(relx=0,rely=0.6,relwidth=1,relheight=0.15)
+btn_battery = tk.Button(root, text="Get Battery", bg="violet")
+btn_battery["font"] = myFont1
+btn_battery.place(relx=0,rely=0.6,relwidth=1,relheight=0.15)
+btn_battery.bind("<Button-1>",get_battery)
 
 #EStop
-btn_control_type = Radiobutton(root, text = "Emergency Stop", bg="red", variable=mode, value=0)
-btn_control_type['font'] = myFont1
-btn_control_type.place(relx=0, rely=0.85, relwidth=1, relheight=0.15)
+btn_halt = Radiobutton(root, text = "HALT", bg="red", variable=mode, value=0)
+btn_halt['font'] = myFont1
+btn_halt.place(relx=0, rely=0.85, relwidth=1, relheight=0.15)
 
 root.geometry("500x500")
 root.mainloop()
