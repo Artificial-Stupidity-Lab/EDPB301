f92036500f422043f4a776efa61e84f2ef389edc
Mpilo Mbulelo Tatana <81185746+Artificial-Stupidity-Lab@users.noreply.github.com>
2022-09-07 21:43:40 +0200

    Update droneProjectCode.py

--- Drone_Project_Code/droneProjectCode.py
+++ Drone_Project_Code/droneProjectCode.py
@@ -23,26 +23,34 @@ frame.place(relwidth=1,relheight=1)
 #button styles
 myFont1 = font.Font(family='Helvetica', size=20, weight='bold')
 #GUI variables
-mode = IntVar()
-mode.set(0)
+
 
 #function for clicked button
 def get_battery(event):
-    pass
+    print("\nEntering Battery Mode")
+    #create mesage box
+
+def surveillance_mode(event):
+    print("\nEntering Surveillance Mode")
+    #create mesage box
+
+def read_database(event):
+    print("\nEntering Database Mode")
+    #create mesage box
+
+def tracking_mode(event):
+    print("\nEntering tracking Mode")
+    #create mesage box
+
+def defensive_mode(event):
+    print("\nEntering defensive Mode")
+    #create mesage box
+
+def halt(event):
+    print("\nEntering halt Mode")
     #create mesage box
 
 
-def modeSel():
-    if(mode.get()==0):
-        mode0Win()
-    elif(mode.get()==1):
-        mode1Win()
-    elif(mode.get()==2):
-        mode2Win()
-    elif(mode.get()==3):
-        mode3Win()
-    elif(mode.get()==4):
-        mode4Win()
 
 #opening new windows for the each mode
 def mode0Win():
@@ -59,22 +67,28 @@ def mode4Win():
 
 
 #Surveillance Mode
-btn_surveillance = Radiobutton(root, text="Surveillance", bg="lime", variable=mode, value=1, command=modeSel)
+btn_surveillance = tk.Button(root, text="Surveillance", bg="lime")
 btn_surveillance['font'] = myFont1
 btn_surveillance.place(relx=0, rely=0, relwidth=0.5, relheight=0.2)
-#Voice Control Mode
-btn_voice = Radiobutton(root, text="Voice Control", bg="yellow", variable=mode, value=2,command=modeSel)
-btn_voice['font'] = myFont1
-btn_voice.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.2)
+btn_surveillance.bind("<Button-1>",surveillance_mode)
 
-#Hand Control Mode
-btn_hand = Radiobutton(root, text="Hand Control", bg="orange", variable=mode, value=3,command=modeSel)
-btn_hand['font'] = myFont1
-btn_hand.place(relx=0, rely=0.3, relwidth=0.5, relheight=0.2)
+#Database Mode
+btn_database = tk.Button(root, text="Database", bg="yellow")
+btn_database['font'] = myFont1
+btn_database.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.2)
+btn_database.bind("<Button-1>",read_database)
+
+#Defensive Mode
+btn_defensive = tk.Button(root, text="Defensive", bg="orange")
+btn_defensive['font'] = myFont1
+btn_defensive.place(relx=0, rely=0.3, relwidth=0.5, relheight=0.2)
+btn_defensive.bind("<Button-1>",defensive_mode)
+
 #Tracking Mode
-btn_tracking = Radiobutton(root, text="Tracking", bg="#80c1ff",variable=mode, value=4,command=modeSel)
+btn_tracking = tk.Button(root, text="Tracking", bg="#80c1ff")
 btn_tracking['font'] = myFont1
 btn_tracking.place(relx=0.5, rely=0.3, relwidth=0.5, relheight=0.2)
+btn_tracking.bind("<Button-1>",tracking_mode)
 
 #Battery
 btn_battery = tk.Button(root, text="Get Battery", bg="violet")
@@ -83,9 +97,10 @@ btn_battery.place(relx=0,rely=0.6,relwidth=1,relheight=0.15)
 btn_battery.bind("<Button-1>",get_battery)
 
 #EStop
-btn_halt = Radiobutton(root, text = "HALT", bg="red", variable=mode, value=0)
+btn_halt = tk.Button(root, text = "HALT", bg="red")
 btn_halt['font'] = myFont1
 btn_halt.place(relx=0, rely=0.85, relwidth=1, relheight=0.15)
+btn_halt.bind("<Button-1>",halt)
 
 root.geometry("500x500")
 root.mainloop()
