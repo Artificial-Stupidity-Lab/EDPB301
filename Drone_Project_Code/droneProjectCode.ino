1df66d616fe91aec7bcc4c15f4cd4fb9e292341b
Mpilo Mbulelo Tatana <81185746+Artificial-Stupidity-Lab@users.noreply.github.com>
2022-11-09 00:53:13 +0200

    Update droneProjectCode.ino

--- Drone_Project_Code/droneProjectCode.ino
+++ Drone_Project_Code/droneProjectCode.ino
@@ -20,7 +20,13 @@ int Bpin2=21;
 int Yval2;
 int Xval2;
 int Bval2;
-
+//pb variables
+const int pb_take_Pic_pin = 10;
+int TakePicture;
+int exit;
+const int pb_exit_pin = 11;
+//led variables
+const int indicator_led = 10;
 
 void setup(){
 
@@ -47,6 +53,9 @@ listen(){
 //init arduino
 void initArduino(){
     Serial.begin(115200);
+    pinMode(indicator_led, OUTPUT);
+    pinMode(pb_take_Pic,INPUT);
+    pinMode(pb_exit,INPUT);
 }
 
 void surveillanceMoves(){
@@ -56,56 +65,78 @@ void surveillanceMoves(){
     Yval2=analogRead(Ypin2);
     Bval1=digitalRead(Bpin1);
     Bval2=digitalRead(Bpin2);
+    TakePicture=digitalRead(pb_take_Pic_pin);
+    exit=digitalRead(pb_exit_pin);
     if(Xval1==0){
         //tell python to go left
         talk(left);
+        notify();
     }
     if(Xval1==1023){
         //tell python to go right
         talk(right);
+        notify();
     }
     if(Xval2==0){
         //tell pyton to rotate left
         talk(antiClockwise);
+        notify();
     }
     if(Xval2==1023){
         //tell python to rotate right
         talk(clockwise);
+        notify();
     }
     if(Bval1==0){
         //tell py to speed up
         talk(speedUp);
+        notify();
     }
     if(Bval2==0){
         //tell py to slow down
         talk(speedDown);
+        notify();
     }
     if(Yval1==0){
         //tell python to go back
         talk(back);
+        notify();
     }
     if(Yval1==1023){
         //tell python to go forward
         talk(forward);
+        notify();
     }
     if(Yval2==0){
         //tell pyton to move down
         talk(down);
+        notify();
     }
     if(Xval2==1023){
-        //tell python to move down
+        //tell python to move up
         talk(up);
+        notify();
     }
     if(Bval1==0 && Bval2==0){
         //tell drone to land or go up
         talk(landTakeoff);
+        notify();
         delay(3000);
     }
-    if(TakePicture==1){
+    if(TakePicture==0){
+        //big PB
         talk(takePic);
+        notify();
+
+    if(exit==0){
+        //samll pb
+        talk(exit);
+        notify();
+    }
     }
     if(Bval1==1 & Bval2==1){
         radar();
+        notify();
     }
     else(){
         talk(none);
@@ -123,4 +154,10 @@ void getWeather(){
     //reading the barometer reading
     //may have to remove
 
+}
+void notify(){
+    digitalWrite(indicator_led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(500);                       // wait for a second
+    digitalWrite(indicator_led, LOW);    // turn the LED off by making the voltage LOW
+    delay(500);  
 }
